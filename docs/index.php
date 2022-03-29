639fba5d471b0a63d436ad484de78472ba474650
Griffin Riley <griffin@10-116-22-183.wireless.oregonstate.edu>
2022-03-29 13:28:04 -0700

    added table for names and ratings

--- docs/index.php
+++ docs/index.php
@@ -1,3 +1,12 @@
 <?php include "header.php"; ?>
-The Strange Case of Starship Iris
+<table>
+    <tr>
+        <th>Name</th>
+        <th>Rating</th>
+    </tr>
+    <tr>
+        <th>The Strange Case of Starship Iris</th>
+        <th>10/10</th>
+    </tr>
+<table>
 <?php include "footer.php"; ?>
